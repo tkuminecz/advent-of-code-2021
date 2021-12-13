ba2fc3eacdb4254213138bc22190926f47c97324
Tim Kuminecz <tkuminecz@gmail.com>
2021-12-13 00:46:47 -0500

    day 13 part 1

--- src/helpers.mjs
+++ src/helpers.mjs
@@ -1,5 +1,5 @@
-import { once } from 'events';
-import readline from 'readline';
+import { once } from "events";
+import readline from "readline";
 
 export const input = readline.createInterface({
   input: process.stdin,
@@ -7,7 +7,15 @@ export const input = readline.createInterface({
 
 export const getLines = async () => {
   const lines = [];
-  input.on('line', line => lines.push(line));
-  await once(input, 'close');
+  input.on("line", (line) => lines.push(line));
+  await once(input, "close");
   return lines;
-}
+};
+
+export const multiArray = (rows, cols, fill = null) => {
+  return new Array(rows)
+    .fill(null)
+    .map((row) =>
+      new Array(cols).fill(typeof fill == "function" ? fill() : fill)
+    );
+};
